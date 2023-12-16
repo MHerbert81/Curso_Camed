08319c0ceab6043b8153511f2951c83b90d3dcf3
Bruno <bm0601789@gmail.com>
2023-12-16 11:54:21 -0300

    implementacao da api get/id

--- healthcare-main/src/Healthcare.Api/Program.cs
+++ healthcare-main/src/Healthcare.Api/Program.cs
@@ -1,25 +1,51 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+using Healthcare.Api.Builders;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Healthcare.Api
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            
+            builder.Services.AddCors(cors => 
+            {
+                cors.AddDefaultPolicy(policy => 
+                {
+                    policy.SetIsOriginAllowedToAllowWildcardSubdomains()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowAnyOrigin()
+                          .Build();
+                });
+            });
+                    
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseApplyMigrations();
+            app.UseHttpsRedirection();
+            
+            app.UseCors();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
